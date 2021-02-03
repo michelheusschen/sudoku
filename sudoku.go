d7694ea135f5eb7be0bf333498e5bdfa12596a62
Michel Heusschen <michelheusschen@gmail.com>
2021-02-03 09:19:54 +0100

    Use constants for sudoku size

--- sudoku.go
+++ sudoku.go
@@ -5,24 +5,35 @@ import (
 	"strings"
 )
 
+const (
+	// Size of the sudoku. The size also affects the range of possible digits.
+	// Maximum size is 30 to prevent int32 overflow. The chosen size must also
+	// be a perfect square root, due to a limitation in the implementation.
+	// This means only the sizes 4, 9, 16 and 25 are supported.
+	Size = 9
+
+	// BoxSize is the square root of Size.
+	BoxSize = 3
+)
+
 // Sudoku contains the data structure needed to solve the puzzle.
 type Sudoku struct {
 	// Grid contains the numbers as provided by the user. This grid
 	// is manipulated directly when solving the sudoku.
-	Grid [9][9]int
+	Grid [Size][Size]int
 
 	// Box, Row and Column digits are stored in an int for fast
 	// changes and checks for a certain digit with bitwise operators.
-	boxDigits    [3][3]int
-	rowDigits    [9]int
-	columnDigits [9]int
+	boxDigits    [BoxSize][BoxSize]int
+	rowDigits    [Size]int
+	columnDigits [Size]int
 
 	// Amount of steps for solving the sudoku.
 	StepCount uint64
 }
 
 // New creates a new sudoku.
-func New(grid [9][9]int) *Sudoku {
+func New(grid [Size][Size]int) *Sudoku {
 	return &Sudoku{Grid: grid}
 }
 
@@ -30,10 +41,10 @@ func New(grid [9][9]int) *Sudoku {
 // if that number is allowed. This is done recursively until the sudoku
 // is solved or until the sudoku is deemed unsolvable.
 func (s *Sudoku) backTrack(row, column int) bool {
-	if row == 9 {
+	if row == Size {
 		return true
 	}
-	if column == 9 {
+	if column == Size {
 		return s.backTrack(row+1, 0)
 	}
 
@@ -42,19 +53,19 @@ func (s *Sudoku) backTrack(row, column int) bool {
 		s.StepCount++
 
 		// Loop through the possible digits.
-		for i := 1; i <= 9; i++ {
+		for i := 1; i <= Size; i++ {
 			bit := 1 << (i - 1)
 
 			// Check with bitwise operators if the digit is still needed
 			// in a part of the sudoku.
-			boxNeedsDigit := s.boxDigits[row/3][column/3]&bit == 0
+			boxNeedsDigit := s.boxDigits[row/BoxSize][column/BoxSize]&bit == 0
 			rowNeedsDigit := s.rowDigits[row]&bit == 0
 			columnNeedsDigit := s.columnDigits[column]&bit == 0
 
 			// Only set digit when box, row and column needs the digit.
 			if boxNeedsDigit && rowNeedsDigit && columnNeedsDigit {
 				// Set digit.
-				s.boxDigits[row/3][column/3] |= bit
+				s.boxDigits[row/BoxSize][column/BoxSize] |= bit
 				s.rowDigits[row] |= bit
 				s.columnDigits[column] |= bit
 				s.Grid[row][column] = i
@@ -66,7 +77,7 @@ func (s *Sudoku) backTrack(row, column int) bool {
 				}
 
 				// Digit was incorrect, remove.
-				s.boxDigits[row/3][column/3] &^= bit
+				s.boxDigits[row/BoxSize][column/BoxSize] &^= bit
 				s.rowDigits[row] &^= bit
 				s.columnDigits[column] &^= bit
 				s.Grid[row][column] = 0
@@ -84,12 +95,12 @@ func (s *Sudoku) backTrack(row, column int) bool {
 // contains the end result.
 func (s *Sudoku) Solve() bool {
 	// Initialize box, row and column bits.
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
+	for i := 0; i < Size; i++ {
+		for j := 0; j < Size; j++ {
 			if digit := s.Grid[i][j]; digit > 0 {
 				bit := 1 << (digit - 1)
 
-				s.boxDigits[i/3][j/3] |= bit
+				s.boxDigits[i/BoxSize][j/BoxSize] |= bit
 				s.rowDigits[i] |= bit
 				s.columnDigits[j] |= bit
 			}
